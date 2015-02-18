4a48fb713dc131f02dba18656d1b18b93a06cada
A H <an.ho@huawei.com>
2015-02-18 13:41:21 -0800

    Initial yang models for usc.
    Change-Id: If71e42fa716be4bf6f338c404aa1af8061ef7f5e
    Signed-off-by: A H <an.ho@huawei.com>

--- usc-impl/src/main/java/org/opendaylight/usc/UscProvider.java
+++ usc-impl/src/main/java/org/opendaylight/usc/UscProvider.java
@@ -8,22 +8,27 @@
 package org.opendaylight.usc;
 
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.RpcRegistration;
 import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.usc.rev150101.UscService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class UscProvider implements BindingAwareProvider, AutoCloseable {
 
     private static final Logger LOG = LoggerFactory.getLogger(UscProvider.class);
-
+    private RpcRegistration<UscService> uscService;
+    
     @Override
     public void onSessionInitiated(ProviderContext session) {
         LOG.info("UscProvider Session Initiated");
+        uscService = session.addRpcImplementation(UscService.class, new UscServiceImpl());
     }
 
     @Override
     public void close() throws Exception {
         LOG.info("UscProvider Closed");
+        uscService.close();
     }
 
 }
